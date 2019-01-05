eaa5e984f067126fc791bb40e99ed15ec94d3631
Christian281 <46402435+Christian281@users.noreply.github.com>
2019-01-05 17:22:47 +0100

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -11,34 +11,51 @@ from random import randint
 
 class ClickerCell(Widget):
     weight = NumericProperty(100)
+    fade_factor =  2
+    growth_factor = 1
+    fade_list = [15,30,45,60,75,90]
+    dead=""
     
     def grow(self):
-        self.weight +=1
-        self.center = self.center_x- 0.5, self.center_y - 0.5
+        self.weight += self.growth_factor
+        self.center = self.center_x- self.growth_factor/2, self.center_y - self.growth_factor/2
         print(self.center_x, self.center_y)
 
 
     def fade(self):
         if self.weight > 0:
-            self.weight -=7
-            self.center = self.center_x+ 3.5, self.center_y + 3.5
+            if self.weight-self.fade_factor > 0:
+                self.weight -= self.fade_factor
+                self.center = self.center_x + self.fade_factor/2, self.center_y + self.fade_factor/2
+            else:
+                self.weight = 0
             
 
 class ClickerGame(Widget):
+
     cell = ObjectProperty(None)
     feed = ObjectProperty(None)
     timer = NumericProperty(0)
+
     def update(self, dt):
+        self.timer += 1
+        if self.timer in self.cell.fade_list:
+            self.cell.fade_factor +=1
         if self.timer % 2 == 0:
             self.cell.fade()
-        self.timer += 1
+        if self.cell.weight == 0:
+            self.cell.dead="Game Over"
+        print(self.cell.weight)
+        print(self.cell.dead)
+        print(self.cell.center)
+        
 
 
 class ClickerApp(App):
     def build(self):
         self.game= ClickerGame()
         self.game.feed.bind(on_press=self.grow)
-        Clock.schedule_interval(self.game.update, 1)
+        Clock.schedule_interval(self.game.update, int(1))
         return self.game
 
     def grow(self, obj):

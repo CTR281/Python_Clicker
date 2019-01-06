0544896481f3c94048528582cd2fc11cb7752315
Christian281 <46402435+Christian281@users.noreply.github.com>
2019-01-06 11:20:17 +0100

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -4,44 +4,35 @@ kivy.require('1.10.1')
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty,StringProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
 from random import randint
 
-
 class Enemy(Widget):
-    velocity_x = NumericProperty(0)
+    velocity_x = NumericProperty(4)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
     enemy_weight=randint(2,10)
-    enemy_size=50,50
+    enemy_weight_temp = NumericProperty(enemy_weight*5+20)
+    enemy_size=ReferenceListProperty(enemy_weight_temp, enemy_weight_temp)
+    pos_x= NumericProperty(5)
+    pos_y=NumericProperty(randint(210,790))
+    pos=ReferenceListProperty(pos_x,pos_y)
 
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
 
     def collide(self,cell,game):
         if self.collide_widget(cell):
-            cell.weight -= self.weight
-            game.remove_widget(self)
+            cell.cell_weight -= self.enemy_weight
+            game.children.remove_widget(self)
 
-
-class AutoClicker(Widget):
-    feed_per_second = NumericProperty(0)
-    amount = NumericProperty(0)
-    buy_cost = NumericProperty(0)
-    tier = NumericProperty(0)
-
-    def __init__(self, tier, **kwargs):
-        super(AutoClicker, self).__init__(**kwargs)
-        self.tier = tier
-    def autofeed(self):
-        return self.feed_per_second * self.amount
 
 
 class ClickerCell(Widget):
     cell_weight = NumericProperty(101)
-    fade_factor =  50
+    fade_factor =  2
     growth_factor = 1
     fade_list = [15*k for k in range(100)]
     cell_size=ReferenceListProperty(cell_weight,cell_weight)
@@ -62,57 +53,100 @@ class ClickerCell(Widget):
                 self.cell_weight = 1
                 self.pos= game.width * 3 / 8, game.height * 5 / 8
 
-    def autofeed(self, autoclicker):
-        self.cell_weight += autoclicker.autofeed()
+
             
 
 class ClickerGame(Widget):
 
     cell = ObjectProperty(None)
     feed = ObjectProperty(None)
-    auto_tier1 = AutoClicker(1)
     timer = NumericProperty(0)
-    gameover=""
-    enemy_list = []
+    gameover=StringProperty("")
+    enemy_list=[]
+    count = 0
     phase2 = False
 
     def spawn_enemy(self):
-        enemy = Enemy()
+        enemy=Enemy()
+        enemy.enemy_weight=randint(2,10)
+        enemy.pos=5, randint(210,400)
+        enemy.velocity = Vector(5,0).rotate(randint(-90,90))
+
         self.add_widget(enemy)
-        return enemy
+        self.enemy_list.append(enemy)
+
+    def bounce(self,enemy):
+        # bounce off top and bottom
+        if (enemy.pos_y < self.height* 1 / 4) or (enemy.pos_y+enemy.enemy_size[0] > self.height):
+            enemy.velocity_y *= -1
+
+        # bounce off left and right
+        if (enemy.pos_x < 0) or (enemy.pos_x+enemy.enemy_size[0] > self.width * 3 / 4):
+            enemy.velocity_x *= -1
 
 
     def update(self, dt):
-        self.timer += 1
-        if self.timer > 5:
-            self.phase2= True
-        if self.timer in self.cell.fade_list:
-            self.cell.fade_factor +=1
-        if self.timer % 2 == 0:
-            self.cell.fade(self)
-        if self.phase2:
-            if self.timer % 5 == 0:
-                enemy=self.spawn_enemy()
-                self.enemy_list.append(enemy)
-            for enemy in self.enemy_list:
-                enemy.collide(self.cell, self)
-                enemy.move()
-        if self.cell.cell_weight == 1:
-            self.gameover="Game Over"
+        self.count +=1
+        if self.count % 60 == 0:
+            self.timer +=1
+            if self.timer >0:
+                self.phase2= True
+            if self.timer in self.cell.fade_list:
+                self.cell.fade_factor +=1
+            if int(self.timer) % 2 == 0:
+                self.cell.fade(self)
+            if self.phase2:
+                if int(self.timer) % 5 == 0:
+                    self.spawn_enemy()
+            if self.cell.cell_weight == 1:
+                self.gameover="Game Over" 
+            print(self.enemy_list)
+            print(self.height)
+            print(self.width)
 
-        self.cell.autofeed(self.auto_tier1)
+
+
+
+
+        for enemy in self.enemy_list:
+            enemy.move()
+            enemy.collide(self.cell, self)
+            self.bounce(enemy)
+
+            #print(self.enemy_list[0].pos)
+
+            #print((self.enemy_list[0].x < 0) or (self.enemy_list[0].right > self.width))
+
+            #print(self.enemy_list[0].x)
+
+            #print(self.enemy_list[0].right)
+
+
+
+
+
         
-        print(self.cell.cell_weight == 1)
-        print(self.gameover)
-        #print(self.cell.center)
+
+
+
+
+
+
+        #print(int(self.timer*10000)+1)
+        #print(int(self.timer+1)*10000)
+        #print(self.timer)
 
 
 class ClickerApp(App):
     def build(self):
         self.game= ClickerGame()
-        Clock.schedule_interval(self.game.update, int(1))
+        Clock.schedule_interval(self.game.update, 1/60)
         return self.game
 
+
+
+
+        
 
 if __name__ == '__main__':
     ClickerApp().run()
